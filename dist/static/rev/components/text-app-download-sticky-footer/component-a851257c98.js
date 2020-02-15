258e09db2de78291cc1da52dedf67606d67216d5
daria07 <d.ilina07@yandex.ru>
2020-02-15 21:07:44 +0300

    init

---
+++ dist/static/rev/components/text-app-download-sticky-footer/component-a851257c98.js
@@ -0,0 +1,2 @@
+"use strict";ebates.stickyFooter=ebates.stickyFooter||function(){function o(o,t,e,i){$(window).on("scroll",function(){var n=$(window).scrollTop();n>o?e.fadeIn():e.fadeOut(),n>t?i.addClass("fix"):i.removeClass("fix")})}return{init:o}}();
+//# sourceMappingURL=component-a851257c98.js.map
